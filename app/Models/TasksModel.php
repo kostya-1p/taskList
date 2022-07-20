8a01f18eca6b2c493841b252f0af3a3371e7e884
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2022-07-20 08:08:17 +0400

    Deleted unnecessary model task array

--- app/Models/TasksModel.php
+++ app/Models/TasksModel.php
@@ -4,13 +4,11 @@ namespace App\Models;
 
 class TasksModel
 {
-    private $tasks = [];
 
     public function getTasks(): array
     {
         if (isset($_COOKIE['tasks'])) {
-            $this->tasks = json_decode($_COOKIE['tasks'], true);
-            return $this->tasks;
+            return json_decode($_COOKIE['tasks'], true);
         }
 
         return [];
