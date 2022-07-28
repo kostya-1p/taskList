886ec2f7aadbb8cdf14e48e964a5bc6ede9e406a
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2022-07-28 14:01:06 +0400

    Tables are created when app builds

--- docker/init.sql
+++ docker/init.sql
@@ -1,6 +1,21 @@
 CREATE DATABASE tasklist;
 USE tasklist;
 
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+                         `id` int NOT NULL AUTO_INCREMENT,
+                         `login` varchar(50) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+                         PRIMARY KEY (`id`),
+                         UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+LOCK TABLES `users` WRITE;
+
+UNLOCK TABLES;
+
 DROP TABLE IF EXISTS `tasks`;
 
 CREATE TABLE tasks (
@@ -15,19 +30,4 @@ CREATE TABLE tasks (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 LOCK TABLES `tasks` WRITE;
-UNLOCK TABLES;
-
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `login` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-LOCK TABLES `users` WRITE;
-
 UNLOCK TABLES;
